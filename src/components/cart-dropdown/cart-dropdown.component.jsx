6967e83007e7b3180cac7b2178b5257ebd459209
ericmugo1 <35067862+ericmugo1@users.noreply.github.com>
2020-05-04 19:15:10 +0200

    moved cart dropdown customButton out of cart-items div and deleted extra line in header component

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,10 +8,10 @@ const CartDropdown = ({ cartItems }) => (
     <div className='cart-dropdown'>
         <div className='cart-items'>
             { cartItems.map(cartItem =>
-                <CartItem key={ cartItem.id } item={ cartItem } />)
-            }
-            <CustomButton> GO TO CHECKOUT</CustomButton>
+                <CartItem key={ cartItem.id } item={ cartItem } />
+            ) }
         </div>
+        <CustomButton> GO TO CHECKOUT</CustomButton>
     </div>
 );
 
